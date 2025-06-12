05170e20337dae63df8a5d33a58a7e5ca309f36d
백경석\seok <dno06117@naver.com>
2025-06-12 14:41:12 +0900

    momo first commit

---
+++ momo/src/test/java/challengers/momo/MomoApplicationTests.java
@@ -0,0 +1,13 @@
+package challengers.momo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
